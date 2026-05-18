75b176cf423ff561821fa9d7bbe19e2c43b9a4f5
Laura Santiago <laurasantym1412@gmail.com>
2026-05-18 14:34:35 -0500

    Agregue las pantallas login, recuperar contraseña y email enviado

---
+++ src/routes/AppRouter.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LoginScreen from '../pages/LoginScreen';
+import ForgotPasswordScreen from '../pages/ForgotPasswordScreen';
+
+const AppRouter = () => {
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => setTheme(t => t === 'light' ? 'dark' : 'light');
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={
+          <LoginScreen theme={theme} onToggleTheme={toggleTheme} />
+        } />
+        <Route path="/forgot-password" element={
+          <ForgotPasswordScreen theme={theme} onToggleTheme={toggleTheme} />
+        } />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
