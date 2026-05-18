36fccb28323a5e47ddf792eca1d4b105d8bd01eb
Laura Santiago <laurasantym1412@gmail.com>
2026-05-18 14:48:40 -0500

    Arregle errores

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import LoginScreen from '../pages/LoginScreen';
 import ForgotPasswordScreen from '../pages/ForgotPasswordScreen';
+import EmailSentScreen from '../pages/EmailSentScreen';
+import RegisterScreen from '../pages/RegisterScreen';
 
 const AppRouter = () => {
   const [theme, setTheme] = useState('light');
@@ -11,12 +13,10 @@ const AppRouter = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={
-          <LoginScreen theme={theme} onToggleTheme={toggleTheme} />
-        } />
-        <Route path="/forgot-password" element={
-          <ForgotPasswordScreen theme={theme} onToggleTheme={toggleTheme} />
-        } />
+        <Route path="/login" element={<LoginScreen theme={theme} onToggleTheme={toggleTheme} />} />
+        <Route path="/forgot-password" element={<ForgotPasswordScreen theme={theme} onToggleTheme={toggleTheme} />} />
+        <Route path="/forgot-password/sent" element={<EmailSentScreen theme={theme} onToggleTheme={toggleTheme} />} />
+        <Route path="/register" element={<RegisterScreen theme={theme} onToggleTheme={toggleTheme} />} />
       </Routes>
     </BrowserRouter>
   );
