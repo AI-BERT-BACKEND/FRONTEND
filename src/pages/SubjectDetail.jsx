035d74b0e8010c57b33409a7d77eeadee744186e
Laura Santiago <laurasantym1412@gmail.com>
2026-05-20 02:01:31 -0500

    Agregue mas pantalals de materias y detalles de cortes

--- src/pages/SubjectDetail.jsx
+++ src/pages/SubjectDetail.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import AppLayout from '../components/Layout/AppLayout';
 import ProgressBar from '../components/ProgressBar';
 import { useTheme } from '../context/ThemeContext';
